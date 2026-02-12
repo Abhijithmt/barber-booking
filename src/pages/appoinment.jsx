7bb1c2c380bf91ee6d93780ede080849049c1dcb
abhijith <abhijithmtmkm@gmail.com>
2026-02-12 14:00:26 +0530

    facial completed

--- src/pages/appoinment.jsx
+++ src/pages/appoinment.jsx
@@ -2,6 +2,10 @@ import basehair from '../assets/basic hair cut.png'
 import advance from '../assets/advance haircut.jpg'
 import premium from '../assets/premium1cut.jpg'
 import kids from '../assets/kids cut.jpg'
+import facial1 from '../assets/facial1.jpg'
+import facial2 from '../assets/facial2.jpg'
+import facial3 from '../assets/facial3.jpeg'
+import facial4 from '../assets/facial4.jpeg'
 function Appoinment(){
     return(
         <>
@@ -15,7 +19,7 @@ function Appoinment(){
            <div className='border border-gray-200 rounded-xl px-4 py-12 w-auto h-80 shadow-sm mx-52 my-8 flex justify-between'>
             <div className="flex flex-col items-center ">
             <img  className="rounded-2xl w-[180px] h-[220px] object-cover"src={basehair} alt='hair1'></img>
-            <p className=' mt-4 font-extrabold'>basic haircut</p>
+            <p className=' mt-4 font-extrabold'>Basic haircut</p>
             </div>
             <div className='flex flex-col items-center'>
             <img className='rounded-2xl w-[180px] h-[220px] object-cover' src={advance} alt='hair2'></img>
@@ -23,11 +27,11 @@ function Appoinment(){
             </div>
             <div className='flex flex-col items-center'>
             <img className="rounded-2xl w-[180px] h-[220px] object-cover"src={premium} alt="hair3" ></img>
-            <p className='mt-4 font-extrabold'>premium haircut</p>
+            <p className='mt-4 font-extrabold'>Premium haircut</p>
             </div>
             <div className='flex flex-col items-center'>
             <img  className="rounded-2xl w-[180px] h-[183px] object-cover"src={kids} alt="hair4"></img> 
-            <p className='mt-4 font-extrabold'>kids haircut</p>
+            <p className='mt-4 font-extrabold'>Kids haircut</p>
             </div>
            </div>
            {/* details of hair cut //basic */}
@@ -105,47 +109,47 @@ function Appoinment(){
             <h2 className="text-4xl font-extrabold">Facial & Skincare</h2>
             <p className="text-gray-500">Choose the best facial treatment for your skin</p>
            </div>
-          {/* hair cut images */}
+          {/* facial images */}
            <div className='border border-gray-200 rounded-xl px-4 py-12 w-auto h-80 shadow-sm mx-52 my-8 flex justify-between'>
             <div className="flex flex-col items-center ">
-            <img  className="rounded-2xl w-[180px] h-[220px] object-cover"src={basehair} alt='hair1'></img>
-            <p className=' mt-4 font-extrabold'>basic haircut</p>
+            <img  className="rounded-2xl w-[180px] h-[220px] object-cover"src={facial1} alt='hair1'></img>
+            <p className=' mt-4 font-extrabold'>Basic facial</p>
             </div>
             <div className='flex flex-col items-center'>
-            <img className='rounded-2xl w-[180px] h-[220px] object-cover' src={advance} alt='hair2'></img>
-            <p className='mt-4 font-extrabold'>Advance haircut</p>
+            <img className='rounded-2xl w-[180px] h-[220px] object-cover' src={facial2} alt='hair2'></img>
+            <p className='mt-4 font-extrabold'>Advance facial</p>
             </div>
             <div className='flex flex-col items-center'>
-            <img className="rounded-2xl w-[180px] h-[220px] object-cover"src={premium} alt="hair3" ></img>
-            <p className='mt-4 font-extrabold'>premium haircut</p>
+            <img className="rounded-2xl w-[180px] h-[220px] object-cover"src={facial3} alt="hair3" ></img>
+            <p className='mt-4 font-extrabold'>Premium facial</p>
             </div>
             <div className='flex flex-col items-center'>
-            <img  className="rounded-2xl w-[180px] h-[183px] object-cover"src={kids} alt="hair4"></img> 
-            <p className='mt-4 font-extrabold'>kids haircut</p>
+            <img  className="rounded-2xl w-[180px] h-[183px] object-cover"src={facial4} alt="hair4"></img> 
+            <p className='mt-4 font-extrabold'>skincare facial</p>
             </div>
            </div>
-           {/* details of hair cut //basic */}
+           {/* details of basic facial */}
            <div className=' px-20 py-12 w-auto h-auto  mx-52 my-8 grid grid-cols-2 gap-y-6'>
             <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
-                <h3 className='font-extrabold'>Basic haircut</h3>
+                <h3 className='font-extrabold'>Basic facial</h3>
                 <div className='flex text-center gap-2.5'>
-                <h2 className='font-extrabold  text-blue-500 '>₹299</h2>
+                <h2 className='font-extrabold  text-blue-500 '>₹499</h2>
                 <p className='text-gray-400 text-xs mt-1 '>30 min</p>
                 </div>
-                <p className='text-gray-400 text-xs font-sans font-bold'>Regular office / casual style</p>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Normal skin cleaning & refreshing</p>
                 <div className='flex justify-center mt-3.5 '>
                 <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
                 </div>
 
             </div>
-            {/* advance haircut */}
+            {/* advance facial */}
             <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
-                <h3 className='font-extrabold'>Advanced haircut </h3>
+                <h3 className='font-extrabold'>Advanced facial</h3>
                 <div className='flex text-center gap-2.5'>
-                <h2 className='font-extrabold  text-blue-500 '>₹399</h2>
-                <p className='text-gray-400 text-xs mt-1 '>40 min</p>
+                <h2 className='font-extrabold  text-blue-500 '>₹699</h2>
+                <p className='text-gray-400 text-xs mt-1 '>45 min</p>
                 </div>
-                <p className='text-gray-400 text-xs font-sans font-bold'>Fade, layer & modern looks</p>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Deep cleaning & glow treatment</p>
                 <div className='flex justify-center mt-3.5 '>
                 <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
                 </div>
@@ -153,12 +157,12 @@ function Appoinment(){
             </div>
             {/* premium */}
              <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
-                <h3 className='font-extrabold'>premium haircut</h3>
+                <h3 className='font-extrabold'>premium  facial</h3>
                 <div className='flex text-center gap-2.5'>
-                <h2 className='font-extrabold  text-blue-500 '>₹499</h2>
-                <p className='text-gray-400 text-xs mt-1 '>50 min</p>
+                <h2 className='font-extrabold  text-blue-500 '>₹999</h2>
+                <p className='text-gray-400 text-xs mt-1 '>60 min</p>
                 </div>
-                <p className='text-gray-400 text-xs font-sans font-bold'>Complete styling & finish</p>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Complete skin care with massage & mask</p>
                 <div className='flex justify-center mt-3.5 '>
                 <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
                 </div>
@@ -166,12 +170,108 @@ function Appoinment(){
             </div>
             {/* kids */}
              <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
-                <h3 className='font-extrabold'>Kids haircut</h3>
+                <h3 className='font-extrabold'>skincare faical</h3>
                 <div className='flex text-center gap-2.5'>
-                <h2 className='font-extrabold  text-blue-500 '>₹199</h2>
-                <p className='text-gray-400 text-xs mt-1 '>25 min</p>
+                <h2 className='font-extrabold  text-blue-500 '>₹899</h2>
+                <p className='text-gray-400 text-xs mt-1 '>50 min</p>
                 </div>
-                <p className='text-gray-400 text-xs font-sans font-bold'>Normal and Modern for kids</p>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Treatment for acne & oily skin</p>
+                <div className='flex justify-center mt-3.5 '>
+                <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
+                </div>
+
+            </div>
+
+           </div>
+        </section>
+
+
+
+
+
+
+
+
+
+        {/* beard grooming */}
+
+
+
+
+
+
+        <section className="h-auto  py-5 bg-white">
+            <div className="text-center">
+            <h2 className="text-4xl font-extrabold">Facial & Skincare</h2>
+            <p className="text-gray-500">Choose the best facial treatment for your skin</p>
+           </div>
+          {/* facial images */}
+           <div className='border border-gray-200 rounded-xl px-4 py-12 w-auto h-80 shadow-sm mx-52 my-8 flex justify-between'>
+            <div className="flex flex-col items-center ">
+            <img  className="rounded-2xl w-[180px] h-[220px] object-cover"src={facial1} alt='hair1'></img>
+            <p className=' mt-4 font-extrabold'>Basic facial</p>
+            </div>
+            <div className='flex flex-col items-center'>
+            <img className='rounded-2xl w-[180px] h-[220px] object-cover' src={facial2} alt='hair2'></img>
+            <p className='mt-4 font-extrabold'>Advance facial</p>
+            </div>
+            <div className='flex flex-col items-center'>
+            <img className="rounded-2xl w-[180px] h-[220px] object-cover"src={facial3} alt="hair3" ></img>
+            <p className='mt-4 font-extrabold'>Premium facial</p>
+            </div>
+            <div className='flex flex-col items-center'>
+            <img  className="rounded-2xl w-[180px] h-[183px] object-cover"src={facial4} alt="hair4"></img> 
+            <p className='mt-4 font-extrabold'>skincare facial</p>
+            </div>
+           </div>
+           {/* details of basic facial */}
+           <div className=' px-20 py-12 w-auto h-auto  mx-52 my-8 grid grid-cols-2 gap-y-6'>
+            <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
+                <h3 className='font-extrabold'>Basic facial</h3>
+                <div className='flex text-center gap-2.5'>
+                <h2 className='font-extrabold  text-blue-500 '>₹499</h2>
+                <p className='text-gray-400 text-xs mt-1 '>30 min</p>
+                </div>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Normal skin cleaning & refreshing</p>
+                <div className='flex justify-center mt-3.5 '>
+                <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
+                </div>
+
+            </div>
+            {/* advance  */}
+            <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
+                <h3 className='font-extrabold'>Advanced facial</h3>
+                <div className='flex text-center gap-2.5'>
+                <h2 className='font-extrabold  text-blue-500 '>₹699</h2>
+                <p className='text-gray-400 text-xs mt-1 '>45 min</p>
+                </div>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Deep cleaning & glow treatment</p>
+                <div className='flex justify-center mt-3.5 '>
+                <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
+                </div>
+
+            </div>
+            {/* premium */}
+             <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
+                <h3 className='font-extrabold'>premium  facial</h3>
+                <div className='flex text-center gap-2.5'>
+                <h2 className='font-extrabold  text-blue-500 '>₹999</h2>
+                <p className='text-gray-400 text-xs mt-1 '>60 min</p>
+                </div>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Complete skin care with massage & mask</p>
+                <div className='flex justify-center mt-3.5 '>
+                <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
+                </div>
+
+            </div>
+            {/* kids */}
+             <div className='rounded-2xl border px-2.5 py-2.5 shadow-sm w-2xs h-auto'>
+                <h3 className='font-extrabold'>skincare faical</h3>
+                <div className='flex text-center gap-2.5'>
+                <h2 className='font-extrabold  text-blue-500 '>₹899</h2>
+                <p className='text-gray-400 text-xs mt-1 '>50 min</p>
+                </div>
+                <p className='text-gray-400 text-xs font-sans font-bold'>Treatment for acne & oily skin</p>
                 <div className='flex justify-center mt-3.5 '>
                 <button className='bg-black text-white px-16 py-1 rounded-2xl text-center hover:bg-blue-600 font-sans font-medium'>select</button>
                 </div>
