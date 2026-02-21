362e6e79b290b41745970c1f95ed6ea0a6dc8d6f
abhijith <abhijithmtmkm@gmail.com>
2026-02-21 23:21:32 +0530

    frontend almost completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,14 @@
 import logo from '../assets/org barberq.png'
 import {Link} from 'react-router-dom'
+import {HashLink} from 'react-router-hash-link'
 function Header(){
    return(
-     <div className="header-div mt-3.5 ">
+     <div className="header-div mt-3.5 sticky top-0 z-50 bg-white w-full">
          <img  className ="max-w-20 " src={logo} alt="logo" />
         <div className="nav-links">
         <Link to ="/">Home</Link>
-        <a href="#about">about us</a>
-        <a href="#services">services</a>
+        <HashLink smooth to="/#about">About Us</HashLink>
+        <HashLink smooth to="/#services">service</HashLink>
         <Link to='/contact'>contact</Link>
         </div>
         <div className="button-div">
