eb9f0e67236c4e13d082188b6162753354efb5dd
abhijith <abhijithmtmkm@gmail.com>
2026-02-25 12:46:47 +0530

    feat: organized root folder with frontend and backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+const app =express()
+const port = 3000
+require('dotenv').config()
+
+app.get('/', (req,res)=>{
+    res.send(`server is running in browser`)
+} )
+
+app.listen(port,()=>{
+    console.log(`server is running in http://localhost:${port}`);
+    
+})
+mongoose.connect("mongodb://127.0.0.1:27017/barber-booking")
+.then(()=>console.log('mongodb connected succesfully'))
+.catch(()=>console.log('mongodb connnecction error'))
