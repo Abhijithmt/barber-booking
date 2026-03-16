aebacedfde95f6ee2bdcd65b1de0b5c635d71fec
abhijith <abhijithmtmkm@gmail.com>
2026-03-16 13:38:17 +0530

    dashboard welcome name added

--- barber-booking/src/pages/dashboard.jsx
+++ barber-booking/src/pages/dashboard.jsx
@@ -1,14 +1,31 @@
 import { ArrowRight  } from "lucide-react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 export default function Dashboard(){
+
+
+    const [username,SetUsername]=useState("Guest")
+
+    useEffect(()=>{
+
+        const storedemail=localStorage.getItem('email')
+
+        if(storedemail){
+
+            const name =storedemail.split('@')[0].toUpperCase()
+            SetUsername(name)
+        }
+    },[])
+
+
     return(
-        <div className="bg-black px-6 py-10">
+        <div className="bg-black min-h-screen px-6 py-10">
         <div >
-            <h1 className="text-4xl text-blue-600 font-bold">Welcome! to BarberQ</h1>
+            <h1 className="text-6xl text-blue-600 font-bold">Welcome to BarberQ  !<span className="text-amber-100 gap-4 flex">{username}</span></h1>
             </div>
             <div className=" py-2">
-            <p className="text-gray-600 font-bold ">You have no booking now yet! </p>
+            <p className="text-gray-600 font-bold">You have no booking now yet! </p>
              </div>
             <div className="py-2.5">
   <Link to='/booking-page'><button className="bg-blue-600 text-amber-50 flex items-center gap-2 px-4 py-2 font-bold transition-all active:scale-95 rounded-2xl cursor-pointer">
